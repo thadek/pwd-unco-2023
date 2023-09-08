72ed0dcfb5f02a51cebd95bd37f132ce56d42677
Fran Padilla <francisco.padilla@est.fi.uncoma.edu.ar>
2023-09-08 19:22:24 -0300

    Rellenada la tabla auto

---
+++ tp4/auto.php
@@ -0,0 +1,161 @@
+<?php
+class auto {
+    private $patente;
+    private $marca;
+    private $modelo;
+    private $dniDuenio;
+    private $mensajeOperacion;
+
+    public function __construct(){
+        $this->patente="";
+        $this->marca="";
+        $this->modelo="";
+        $this->dniDuenio="";
+        $this->mensajeOperacion ="";
+    }
+
+    public function setear($patente, $marca, $modelo, $dniDuenio){
+        $this->setPatente($patente);
+        $this->setMarca($marca);
+        $this->setModelo($modelo);
+        $this->setDniDuenio($dniDuenio);
+    }
+
+    public function getPatente(){
+        return $this->patente;
+    }
+
+    public function setPatente($patente){
+        $this->patente = $patente;
+    }
+
+    public function getMarca(){
+        return $this->marca;
+    }
+
+    public function setMarca($marca){
+        $this->marca = $marca;
+    }
+
+    public function getModelo(){
+        return $this->modelo;
+    }
+
+    public function setModelo($modelo){
+        $this->modelo = $modelo;
+    }
+
+    public function getDniDuenio(){
+        return $this->dniDuenio;
+    }
+
+    public function setDniDuenio($dniDuenio){
+        $this->dniDuenio = $dniDuenio;
+    }
+
+    public function getMensajeOperacion(){
+        return $this->mensajeOperacion;
+    }
+
+    public function setMensajeOperacion($mensajeOperacion){
+        $this->mensajeOperacion = $mensajeOperacion;
+    }
+
+    public function cargar(){
+        $resp = false;
+        $base=new BaseDatos();
+        $sql="SELECT * FROM auto WHERE patente = ".$this->getPatente();
+        if ($base->Iniciar()) {
+            $res = $base->Ejecutar($sql);
+            if($res>-1){
+                if($res>0){
+                    $row = $base->Registro();
+                    $this->setear($row['patente'], $row['marca'], $row['modelo'], $row['dniDuenio']);
+                }
+            }
+        } else {
+            $this->setMensajeOperacion("auto->listar: ".$base->getError());
+        }
+        return $resp;
+    
+        
+    }
+
+    public function insertar(){
+        $resp = false;
+        $base=new BaseDatos();
+        $sql="INSERT INTO tabla(patente, marca, modelo, dniDuenio)  VALUES (".$this->getPatente().",'".$this->getMarca()."','".$this->getModelo()."','".$this->getDniDuenio()."')";
+        if ($base->Iniciar()) {
+            if ($patente = $base->Ejecutar($sql)) {
+                $this->setPatente($patente);
+                $resp = true;
+            } else {
+                $this->setMensajeoperacion("auto->insertar: ".$base->getError());
+            }
+        } else {
+            $this->setMensajeoperacion("auto->insertar: ".$base->getError());
+        }
+        return $resp;
+    }
+
+    public function modificar(){
+        $resp = false;
+        $base=new BaseDatos();
+        $sql="UPDATE auto SET marca = '".$this->getMarca()."',modelo='".$this->getModelo()."',
+        dniDuenio='".$this->getDniDuenio()."' WHERE patente=". $this->getPatente();
+        if ($base->Iniciar()) {
+            if ($base->Ejecutar($sql)) {
+                $resp = true;
+            } else {
+                $this->setMensajeOperacion("auto->modificar: ".$base->getError());
+            }
+        } else {
+            $this->setMensajeOperacion("auto->modificar: ".$base->getError());
+        }
+        return $resp;
+    }
+
+    public function eliminar(){
+        $resp = false;
+        $base=new BaseDatos();
+        $sql="DELETE FROM auto WHERE patente=".$this->getPatente();
+        if ($base->Iniciar()) {
+            if ($base->Ejecutar($sql)) {
+                return true;
+            } else {
+                $this->setMensajeOperacion("auto->eliminar: ".$base->getError());
+            }
+        } else {
+            $this->setMensajeOperacion("auto->eliminar: ".$base->getError());
+        }
+        return $resp;
+    }
+
+    public static function listar($parametro=""){
+        $arreglo = array();
+        $base=new BaseDatos();
+        $sql="SELECT * FROM auto ";
+        if ($parametro!="") {
+            $sql.='WHERE '.$parametro;
+        }
+        $res = $base->Ejecutar($sql);
+        if($res>-1){
+            if($res>0){
+                
+                while ($row = $base->Registro()){
+                    $obj= new auto();
+                    $obj->setear($row['patente'], $row['marca'], $row['modelo'], $row['dniDuenio']);
+                    array_push($arreglo, $obj);
+                }
+               
+            }
+            
+        } else {
+            $this->setMensajeOperacion("auto->listar: ".$base->getError());
+        }
+ 
+        return $arreglo;
+    }
+}
+
+?>
