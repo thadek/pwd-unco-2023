49fc27811f93060a93192ecd2664a0db21a996e3
MauroFerrada <mauroferrada01@gmail.com>
2023-09-13 15:07:06 -0300

    otros cambios

--- tp4/vista/buscarAuto.php
+++ tp4/vista/buscarAuto.php
@@ -7,7 +7,7 @@
 <body>
     <h1>Buscar Auto por Patente</h1>
 
-    <form action="accionBuscarAuto.php" method="post">
+    <form action="accion/accionBuscarAuto.php" method="post">
         <label for="patente">Número de Patente:</label>
         <input type="text" id="patente" name="patente" required>
         <input type="submit" value="Buscar">
