817bee14e96d557ad2edceb4f589527f7abf9d59
diazfabrici0 <dfabricio255@gmail.com>
2023-09-14 22:06:34 -0300

    estilos a actualizarDatosPersona.php

--- tp4/vista/accion/actualizarDatosPersona.php
+++ tp4/vista/accion/actualizarDatosPersona.php
@@ -3,35 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <title>Actualizar Datos de Persona</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/inicio.css">
+    <script type="text/javascript" src="../js/bootstrap.bundle.min.js"></script>
 </head>
-<body>
-    <h1>Actualizar Datos de Persona</h1>
-
+<body class="bg-dark">
     <?php
- 
 
- require_once("../../../configuracion.php");
-
-    $datos = darDatosSubmitted();
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nroDni'])) {
-        $nroDni = $datos['nroDni'];
-        $nombre = $datos['nombre'];
-        $apellido = $datos['apellido'];
-        $fechaNac = $datos['fechaNac'];
-        $telefono = $datos['telefono'];
-        $domicilio = $datos['domicilio'];
-
-        
-        $abmPersona = new AbmPersona();
-
-        // Modifica los datos de la persona en la base de datos
-        $resultado = $abmPersona->modificarDatosPersona($nroDni, $nombre, $apellido, $fechaNac, $telefono, $domicilio);
-         echo $resultado;
-    } else {
-        echo '<p>No se han proporcionado datos válidos para actualizar.</p>';
-    }
+    require_once("../../../configuracion.php");
+    $rutalogo = "../img/";
+    include_once("../../estructura/menu/menu.php");
+    include_once("../../estructura/Navbar.php");
+    require_once("../../../configuracion.php");
     ?>
 
+<main class="container-fluid cont container text-light">
+
+    <div class="card col-12 text-center" data-bs-theme="dark">
+        <h2>Actualizar Datos de Persona</h2>
+        <div class="container text-center">
+                <?php
+
+                $datos = darDatosSubmitted();
+                if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nroDni'])) {
+                    $nroDni = $datos['nroDni'];
+                    $nombre = $datos['nombre'];
+                    $apellido = $datos['apellido'];
+                    $fechaNac = $datos['fechaNac'];
+                    $telefono = $datos['telefono'];
+                    $domicilio = $datos['domicilio'];
+
+                    
+                    $abmPersona = new AbmPersona();
+
+                    // Modifica los datos de la persona en la base de datos
+                    $resultado = $abmPersona->modificarDatosPersona($nroDni, $nombre, $apellido, $fechaNac, $telefono, $domicilio);
+                    echo $resultado;
+                } else {
+                    echo '<p>No se han proporcionado datos válidos para actualizar.</p>';
+                }
+                ?>  
+        </div>
+    </div>
+</main>
+<div class="contenedor">
+</div>
+<?php include_once("../../estructura/Footer.php"); ?>
 </body>
 </html>
